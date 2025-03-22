bd99a091a1ee93553a111485f8df46210dc61131
Felix "xq" Queißner <git@random-projects.net>
2025-03-22 21:37:21 +0100

    Adds buildable version of byacc

---
+++ vendor/byacc/build.zig
@@ -0,0 +1,93 @@
+const std = @import("std");
+
+const StringArray = []const []const u8;
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const upstream_dep = b.dependency("upstream", .{});
+
+    const byacc_config = b.addConfigHeader(
+        .{
+            .style = .{ .autoconf = upstream_dep.path("config_h.in") },
+            .include_path = "config.h",
+        },
+        .{
+            .GCC_NORETURN = .@"__attribute__((noreturn))",
+            .GCC_PRINTF = 1,
+            .GCC_PRINTFLIKE = .@"__attribute__((format(printf,fmt,var)))",
+            .GCC_SCANF = 1,
+            .GCC_SCANFLIKE = .@"__attribute__((format(scanf,fmt,var)))",
+            .GCC_UNUSED = .@"__attribute__((unused))",
+            .HAVE_FCNTL_H = true,
+            .HAVE_GETOPT = true,
+            .HAVE_GETOPT_H = true,
+            .HAVE_GETOPT_HEADER = true,
+            .HAVE_INTTYPES_H = true,
+            .HAVE_LIBDBMALLOC = null,
+            .HAVE_LIBDMALLOC = null,
+            .HAVE_MEMORY_H = true,
+            .HAVE_MKSTEMP = true,
+            .HAVE_STDINT_H = true,
+            .HAVE_STDLIB_H = true,
+            .HAVE_STDNORETURN_H = true,
+            .HAVE_STRINGS_H = true,
+            .HAVE_STRING_H = true,
+            .HAVE_SYS_STAT_H = true,
+            .HAVE_SYS_TYPES_H = true,
+            .HAVE_UNISTD_H = true,
+            .HAVE_VSNPRINTF = true,
+            .MAXTABLE = null,
+            .MIXEDCASE_FILENAMES = true,
+            .NEED_GETOPT_H = null,
+            .NO_LEAKS = null,
+            .STDC_HEADERS = 1,
+            .STDC_NORETURN = 1,
+            .SYSTEM_NAME = "linux-gnu",
+            .USE_DBMALLOC = null,
+            .USE_DMALLOC = null,
+            .USE_VALGRIND = null,
+            .YYBTYACC = 1,
+            .YY_NO_LEAKS = null,
+            .mode_t = null,
+        },
+    );
+
+    const byacc_exe = b.addExecutable(.{
+        .name = "byacc",
+        .optimize = optimize,
+        .target = target,
+        .link_libc = true,
+    });
+
+    byacc_exe.addConfigHeader(byacc_config);
+    byacc_exe.addCSourceFiles(.{
+        .root = upstream_dep.path("."),
+        .files = byacc_sources,
+        .flags = cflags,
+    });
+
+    b.installArtifact(byacc_exe);
+}
+
+const cflags: []const []const u8 = &.{
+    "--std=gnu11",
+};
+
+const byacc_sources: []const []const u8 = &.{
+    "closure.c",
+    "error.c",
+    "graph.c",
+    "lalr.c",
+    "lr0.c",
+    "main.c",
+    "mkpar.c",
+    "mstring.c",
+    "output.c",
+    "reader.c",
+    "btyaccpar.c", // "$(SKELETON).c",
+    "symtab.c",
+    "verbose.c",
+    "warshall.c",
+};
